8a84fd258fe79ea38a27fa7e2e0854da95bf9d1b
gs12m006 <steffen@kopany.info>
2012-11-12 13:02:19 +0100

    introducing matrices

---
+++ src/crap/types/matrix3.h
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////
+// CRAP Library
+//!		@file matrix3.h
+//
+//	Author(s):
+//! 	@author Steffen Kopany <steffen@kopany.at>
+//
+//	Copyright:
+//!		@copyright Copyright (c) 2012 Steffen Kopany
+//
+//	Description:
+//!		@brief Simple dataholding matrix3 plus general operators
+//
+//	Status (scratch, developed, final):
+//!		@version scratch
+//
+////////////////////////////////////////////////////////
+#pragma once
+
+#ifndef CRAP_MATRIX3_H
+#define CRAP_MATRIX3_H
+
+#include "control/asserts.h"
+
+/*
+ *! @todo iso c11 allows anonymous structs??
+ */
+template<typename T>
+struct matrix3
+{
+
+};
+
+
+
+#endif // CRAP_MATRIX3_H
